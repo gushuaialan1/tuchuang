b0765326c2429d53f9dafe67aad0da3d563cfbc6
gushuaialan1 <gushuaialan@gmail.com>
2025-12-07 17:18:16 +0800

    add text footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,13 +13,7 @@ export default function Footer() {
     <footer className="w-full text-center bg-slate-200 flex flex-col justify-center items-center py-3">
       <div>
         <p className="text-xs text-gray-500">
-          Copyright Ⓒ 2024 All rights reserved. 请勿上传违反中国法律的图片，违者后果自负。 本程序基于Cloudflare Pages，开源于
-          <Link 
-            href="https://github.com/x-dr/telegraph-Image"
-            className="text-blue-300 hover:text-red-900 ml-1"
-            target="_blank"
-            rel="noopener noreferrer"
-          >GitHub Telegraph-Image</Link>
+          Copyright Ⓒ 2024 All rights reserved. 请勿上传违反中国法律的图片，违者后果自负。
         </p>
       </div>
       <div className="flex flex-wrap justify-center gap-3 mt-2">
